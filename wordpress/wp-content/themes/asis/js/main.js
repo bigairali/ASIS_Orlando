38f7c10f4d027c622f9eba93f6b03fdf60e8f177
Chris Whitman <skimalismash@gmail.com>
2012-12-03 08:36:28 -0500

    Fixed bug in jQuery H3 file

--- wordpress/wp-content/themes/asis/js/main.js
+++ wordpress/wp-content/themes/asis/js/main.js
@@ -6,15 +6,16 @@
 	/* For EACH H3 in the ARRAY, find whether there is a SPAN tag in it */
 	title_sections.each( function(index, value){
 		var val = $(value);
-		var is_span = val.find('span').length;
+		var has_title = val.find('span').hasClass('title-section');
+		
 
 		/* IF there is no span tag then make one and put the orginal HTML text in it */
-		if(!is_span){
+		if(!has_title){
 			var val_html = val.html();
 			var new_header = '<span class="title-section">'+ val_html +'</span>';
 			
 			val.html(new_header);
-		}		
+		}
 	});
 	
 })(jQuery);
