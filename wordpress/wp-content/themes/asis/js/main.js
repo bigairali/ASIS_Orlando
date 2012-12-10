4b27870ad3c45d3fd478beefceaae37f5e03d708
Chris Whitman <skimalismash@gmail.com>
2012-12-10 16:30:59 -0500

    new dump and a button

--- wordpress/wp-content/themes/asis/js/main.js
+++ wordpress/wp-content/themes/asis/js/main.js
@@ -1,5 +1,8 @@
 (function($){
 
+	/* Global Variables */
+	var win = $(window);
+
 	/* Find all the H3's on the page and put them in an ARRAY */
 	var title_sections = $("h3");
 	
@@ -18,5 +21,51 @@
 			val.html(new_header);
 		}
 	});
+	
+	
+	
+	
+/* COMMENT ERROR HANDLING */
+
+/* Text Inputs */
+	var author_field = $('#author');
+	var email_field = $('#email');
+	var comment_field = $('#comment');
+	
+/* Form containers */
+	var author_container = $('.comment-form-author');
+	var email_container = $('.comment-form-email');
+	var comment_container = $('.comment-form-comment');
+	
+/* Event Handling */
+	win.on('submit', '#commentform', function(e){
+		var author_text = author_field.val();
+		var email_text = email_field.val();
+		var comment_text = comment_field.val();
+		
+		var is_error_msg = $('.comment_error');
+		
+		
+		if(!comment_text){
+				console.log(is_error_msg[0], 'Error');
+			if(!is_error_msg[0]){
+				
+				comment_field.css('border', '1px solid rgb(222, 0, 16)');
+				
+				html = '<p class="comment_error"><strong>ERROR</strong>: please type a comment.</p>';
+			
+				comment_container.append(html)
+			};
+			
+			
+			return false;			
+		}
+		
+
+	});
+	
+	
+	
+	
 	
 })(jQuery);
