4909b81a8fdde1474a4a99d2105148718d2a5aa2
Chris Whitman <skimalismash@gmail.com>
2012-12-10 19:43:42 -0500

    Fixed the Config

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'loc1233403065910');
+define('DB_NAME', 'wordpress_asis');
 
 /** MySQL database username */
-define('DB_USER', 'loc1233403065910');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Faith1985!');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'loc1233403065910.db.10033528.hostedresource.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
